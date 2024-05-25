d94b567962439e964d1e14fec6b351add16be659
Sahib Muradov <32236360+sahibziko@users.noreply.github.com>
2024-05-26 02:32:06 +0400

    Update inline.py

--- VenomX/plugins/bot/inline.py
+++ VenomX/plugins/bot/inline.py
@@ -36,22 +36,22 @@ async def inline_query_handler(client, query):
                 [
                     [
                         InlineKeyboardButton(
-                            text="ʏᴏᴜᴛᴜʙᴇ 🎄",
+                            text="YouTube 🎄",
                             url=link,
                         )
                     ],
                 ]
             )
             searched_text = f"""
-❄ <b>ᴛɪᴛʟᴇ :</b> <a href={link}>{title}</a>
+❄ <b>Başlıq :</b> <a href={link}>{title}</a>
 
-⏳ <b>ᴅᴜʀᴀᴛɪᴏɴ :</b> {duration} ᴍɪɴᴜᴛᴇs
-👀 <b>ᴠɪᴇᴡs :</b> <code>{views}</code>
-🎥 <b>ᴄʜᴀɴɴᴇʟ :</b> <a href={channellink}>{channel}</a>
-⏰ <b>ᴘᴜʙʟɪsʜᴇᴅ ᴏɴ :</b> {published}
+⏳ <b>müddəti :</b> {duration} ᴍɪɴᴜᴛᴇs
+👀 <b>baxış :</b> <code>{views}</code>
+🎥 <b>kanal :</b> <a href={channellink}>{channel}</a>
+⏰ <b>paylaşıldı :</b> {published}
 
 
-<u><b>➻ ɪɴʟɪɴᴇ sᴇᴀʀᴄʜ ᴍᴏᴅᴇ ʙʏ {app.name}</b></u>"""
+<u><b>➻ inline axtarış modulu {app.name}</b></u>"""
             answers.append(
                 InlineQueryResultPhoto(
                     photo_url=thumbnail,
